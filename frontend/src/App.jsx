6f2947bb5ce4ace8bef2e3a9446065799c594b52
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2024-11-10 18:34:23 +0530

    create navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './Pages/Home'
 import Signin from './Pages/Signin'
 import Signup from './Pages/signup'
+import Navigation from './Components/Navigation'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Navigation />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<Signin />} />
---
+++ frontend/src/Components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Navigation() {
+  return (
+    <nav className="bg-green-900 text-white px-6 py-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
+        <a href="/" className="text-xl font-bold text-green-200">
+          Tea Factory
+        </a>
+        
+        {/* Navigation Links */}
+        <div className="flex space-x-6">
+          <a href="/" className="hover:text-green-400 transition duration-300">Home</a>
+          <a href="/signup" className="hover:text-green-400 transition duration-300">Login</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
