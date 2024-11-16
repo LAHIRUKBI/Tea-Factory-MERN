c051762892dbf89703d1e3108e648b048525f815
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2024-11-16 19:45:17 +0530

    create admin login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import Home from './Pages/Home'
 import Signin from './Pages/Signin'
 import Signup from './Pages/signup'
 import Navigation from './Components/Navigation'
+import Admin_Login from './Pages/Admin_Login'
+import Admin_Home from './Pages/Admin_Home'
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/admin" element={<Admin_Login />} />
+      <Route path="/adminhome" element={<Admin_Home />} />
     </Routes>
     </BrowserRouter>
     
