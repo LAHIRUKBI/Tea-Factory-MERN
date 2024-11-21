47293de601d08141ad7d1f40f9da2896dea6baad
Lahiru illangasinghe <lahiruilangasinha@gmail.com>
2024-11-21 19:10:25 +0530

    create pages and update app.jsx file

---
+++ frontend/src/Pages/Deliverymanager_home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Deliverymanager_home() {
+  return (
+    <div>Deliverymanager_home</div>
+  )
+}
---
+++ frontend/src/Pages/Staffmanager_home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Staffmanager_home() {
+  return (
+    <div>Staffmanager_home</div>
+  )
+}
